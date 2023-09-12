f49a7bcdd219658274623c9eaee6675a656499a0
Radamés <r.alfaro8@icloud.com>
2023-09-11 20:40:53 -0400

    Customize rates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 	let followage = 0;
 	let subscribage = 0;
 
-	let rt_wt = 10;
+	let rt_wt = 100;
 	let rt_fw = 500;
 	let rt_sb = 1000;
 
@@ -13,20 +13,32 @@
 </script>
 
 <main>
-	<h1 class="text-2xl font-bold text-center text-gray-800 dark:text-gray-100 md:text-3xl">DND Converter Thing</h1>
+	<h1 class="text-2xl font-bold text-center text-gray-800 dark:text-gray-100 md:text-3xl">Stream DND Campaign Converter Thing</h1>
 
-	<form class="my-6">
+	<form class="my-6 grid grid-cols-2 gap-4">
 		<div class="flex flex-col text-sm mb-2">
-			<label for="field1" class="font-bold mb-2 text-gray-800 dark:text-gray-100">Hours Watchtime</label>
-			<input type="number" id="field1" bind:value={watchtime} class="appearance-none border border-gray-200 p-2 focus:outline-none focus:border-gray-500 shadow-lg rounded-lg" />
+			<label for="watchtime" class="font-bold mb-2 text-gray-800 dark:text-gray-100">Hours Watchtime</label>
+			<input type="number" id="watchtime" bind:value={watchtime} class="appearance-none border border-gray-200 p-2 focus:outline-none focus:border-gray-500 shadow-lg rounded-lg" />
 		</div>
 		<div class="flex flex-col text-sm mb-2">
-			<label for="field2" class="font-bold mb-2 text-gray-800 dark:text-gray-100">Months Following</label>
-			<input type="number" id="field2" bind:value={followage} class="appearance-none border border-gray-200 p-2 focus:outline-none focus:border-gray-500 shadow-lg rounded-lg" />
+			<label for="rt_wt" class="font-bold mb-2 text-gray-800 dark:text-gray-100">Hours Watchtime Rate</label>
+			<input type="number" id="rt_wt" bind:value={rt_wt} class="appearance-none border border-gray-200 p-2 focus:outline-none focus:border-gray-500 shadow-lg rounded-lg" />
 		</div>
 		<div class="flex flex-col text-sm mb-2">
-			<label for="field3" class="font-bold mb-2 text-gray-800 dark:text-gray-100">Months Subscribed</label>
-			<input type="number" id="field3" bind:value={subscribage} class="appearance-none border border-gray-200 p-2 focus:outline-none focus:border-gray-500 shadow-lg rounded-lg" />
+			<label for="followage" class="font-bold mb-2 text-gray-800 dark:text-gray-100">Months Following</label>
+			<input type="number" id="followage" bind:value={followage} class="appearance-none border border-gray-200 p-2 focus:outline-none focus:border-gray-500 shadow-lg rounded-lg" />
+		</div>
+		<div class="flex flex-col text-sm mb-2">
+			<label for="rt_fw" class="font-bold mb-2 text-gray-800 dark:text-gray-100">Months Following Rate</label>
+			<input type="number" id="rt_fw" bind:value={rt_fw} class="appearance-none border border-gray-200 p-2 focus:outline-none focus:border-gray-500 shadow-lg rounded-lg" />
+		</div>
+		<div class="flex flex-col text-sm mb-2">
+			<label for="subscribage" class="font-bold mb-2 text-gray-800 dark:text-gray-100">Months Subscribed</label>
+			<input type="number" id="subscribage" bind:value={subscribage} class="appearance-none border border-gray-200 p-2 focus:outline-none focus:border-gray-500 shadow-lg rounded-lg" />
+		</div>
+		<div class="flex flex-col text-sm mb-2">
+			<label for="rt_sb" class="font-bold mb-2 text-gray-800 dark:text-gray-100">Months Subscribed Rate</label>
+			<input type="number" id="rt_sb" bind:value={rt_sb} class="appearance-none border border-gray-200 p-2 focus:outline-none focus:border-gray-500 shadow-lg rounded-lg" />
 		</div>
 	</form>
 
